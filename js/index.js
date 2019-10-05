2323d4e8340203aeec4bcdac4d651be8e4dc2be5
Your Name <dunganh388@gmail.com>
2019-10-05 17:43:59 +0700

    lan 1

---
+++ js/index.js
@@ -0,0 +1,105 @@
+﻿$(document).ready(function() {
+	new WOW().init();
+
+	// $('.image-slide').slick({
+ //          infinite: true,
+ //          slidesToShow: 1,
+ //          slidesToScroll: 1,
+ //          autoplay: false,
+ //          dots: true,
+ //          arrows: false,
+ //          autoplaySpeed: 4000
+ //    });
+
+ $('.c-slide03').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: false,
+        asNavFor: '.c-slide03--sub'
+    });
+    $('.c-slide03--sub').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.c-slide03',
+        dots: false,
+        centerMode: false,
+        focusOnSelect: true
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.go-top').fadeIn(200);
+        } else {
+            $('.go-top').fadeOut(200);
+        }
+    });
+    
+    $('.go-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 1000);
+    });
+});
+
+
+// menu
+
+jQuery(document).ready(function($) {
+	 $('.open-nav').click(function(event){
+		$('#menu-mobile').toggleClass('active');
+	});
+
+	$('.btn_exit, .overlay_hide').click(function(){
+		$('#menu-mobile').removeClass('active');
+	});
+});
+
+function myFunction(x) {
+	if (x.matches) {
+		$(function() {
+			$('#menu-mobile').find(function() {
+				$('ul').removeClass('sub_menu_2');
+			});
+		});
+
+		var ccc = $('#menu-mobile ul li').find('ul');
+		if (ccc.length !== 0) {
+			ccc.before('<button class="accordion"></button>');
+			ccc.addClass('sub-menu');
+		}
+		var acc = document.getElementsByClassName("accordion");
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+			acc[i].onclick = function() {
+				this.classList.toggle("active");
+				var panel = this.nextElementSibling;
+				if (panel.style.maxHeight) {
+					panel.style.maxHeight = null;
+				} else {
+		                // panel.style.maxHeight = panel.scrollHeight + "200px";
+		            }
+		        }
+		    };
+
+		$('#menu-mobile').find('.accordion').click(function() {
+			var next = $(this).next();
+		  	next.slideToggle();
+		});
+	} else {
+		$(function() {
+			$('#menu-mobile').find('ul').addClass('sub_menu_2');
+			
+			$('#menu-mobile ul li').find('ul').removeClass('sub-menu');
+		});
+	}
+};
+
+var x = window.matchMedia("(max-width: 1199px)")
+myFunction(x) 
+x.addListener(myFunction) 
+
+
+
+
+
